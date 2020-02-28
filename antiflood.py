00e56bdfe03ad7c2e7b9242e4eca3092d44869f3
Mattia <hackhab@gmail.com>
2020-02-28 17:58:54 +0100

    Added antiflood

--- antiflood.py
+++ antiflood.py
@@ -2,9 +2,24 @@ from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton, Client
 from pyrogram.errors import *
 import time
 import logging
+from collections import defaultdict
+
+
 BANNED_USERS = Filters.chat()
+messages = defaultdict(list)
 
 
 @Client.on_message(Filters.private & ~BANNED_USERS)
-def antiflood(client, message):
-    pass
+def anti_flood(client, message):
+    if len(messages[client.id]) == 7:
+        timestamps = messages.pop(client.id)
+        subtractions = []
+        for index, timestamp in enumerate(timestamps):
+            if index < 6:
+                subtractions.append(timestamp - timestamps[index + 1])
+            else:
+                subtractions.append(timestamps[index - 1] - timestamp)
+        if all(i <= 0.5 for i in subtractions):
+            logging.debug(f"Flood detected from {client.id}")
+    else:
+        messages[client.id].append(message.date)
